898022f47b16e80f5046b902d3fa9be9ce691ab4
Exoalam <nafiul.arpon@gmail.com>
2022-07-09 16:28:17 +0600

    Update

--- main.py
+++ main.py
@@ -12,27 +12,31 @@ def server_receive():
     while True:
         s = socket.socket()
         print("Server Created")
-        port = 55667
+        port = 12344
         s.bind(('', port))
         print("socket binded to %s" % (port))
         s.listen()
         print("socket is listening")
         c, addr = s.accept()
         print('Got connection from', addr)
-        incoming_data = c.recv(6553600).decode()
-        json_file = json.loads(incoming_data)
+        incoming_data = ''
+        while True:
+            incoming_data += c.recv(6553600).decode()
+            if (incoming_data[-1] == '$'):
+                break
+        json_file = json.loads(incoming_data[1:-1])
         image = json_file['data']
-        name = json_file['name']
+        name = json_file['TAG']
         im = PIL.Image.open(BytesIO(base64.b64decode(image)))
         frame, pname = face(im)
-        serve_send(frame, pname)
-        c.close()
-        s.close()
+        c.send(pname.encode())
+        #serve_send(frame, pname)
+        print(pname.encode())
 
 
 def serve_send(frame, pname):
     s = socket.socket()
-    port = 25432
+    port = 12344
     s.connect(('127.0.0.1', port))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     img = PIL.Image.fromarray(frame)
